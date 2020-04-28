91569c4110171abd149b36f1dd55dee4eabf3257
Mochen Yang <mochenyang@users.noreply.github.com>
2020-04-28 15:23:10 -0500

    clean up files

--- R/Hotelling.R
+++
@@ -1,7 +0,0 @@
-# Internal function to compute Hotelling statistic
-hotelling = function(model_IV, model_unbias) {
-  b_diff = stats::coef(model_IV) - stats::coef(model_unbias)
-  var_diff = stats::vcov(model_IV) + stats::vcov(model_unbias)
-  H = as.vector(t(b_diff) %*% solve(var_diff) %*% b_diff)
-  return(H)
-}
